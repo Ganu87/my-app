fbb7dd4c1ab0bfec94ad28e276075b1fa94fcf9b
ganesh shelke <shelke.ganesh87@gmail.com>
2025-01-21 10:20:54 +0530

    4 commit

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`My main Server running on http://localhost:${PORT}`);
 });
