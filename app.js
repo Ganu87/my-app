c24a7315c4ce529bf202c2bf8cacacaae8479b0a
Ganesh Shelke <108321143+Ganu87@users.noreply.github.com>
2025-01-21 16:38:01 +0530

    Revert "error log added"

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.get('/', (req, res) => {
     console.log(" test 1");
     console.log(" dev test 2");
     console.log(" dev test 3");
-    console.log(" ERROR LOG");
 app.listen(PORT, () => {
     console.log(`My main Server running on http://localhost:${PORT}`);
 });
